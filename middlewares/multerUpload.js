0c61a5f7ceed79dce9693e6631be459d74e1f526
njayman <najishm828282@gmail.com>
2021-01-30 19:04:21 +0600

    changed asset save directory

--- middlewares/multerUpload.js
+++ middlewares/multerUpload.js
@@ -1,14 +1,14 @@
-const multer = require('multer')
+const multer = require("multer");
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = file.originalname;
-        cb(null, uniqueSuffix)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "~/assets");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = file.originalname;
+    cb(null, uniqueSuffix);
+  },
+});
 
-const multerUploads = multer({ storage }).single('file');
+const multerUploads = multer({ storage }).single("file");
 
 module.exports = multerUploads;
