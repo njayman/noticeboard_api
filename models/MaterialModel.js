4cb711c4428a9d24f6c9ec30f9461b0cdbba5885
njayman <najishm828282@gmail.com>
2021-05-30 10:06:47 +0600

    added video mute unmute property in material model

--- models/MaterialModel.js
+++ models/MaterialModel.js
@@ -1,22 +1,26 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const MaterialSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    materialtype: {
-        type: String,
-        required: true
-    },
-    material: {
-        type: String,
-        required: true
-    },
-    adminid: {
-        type: String,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  materialtype: {
+    type: String,
+    required: true,
+  },
+  material: {
+    type: String,
+    required: true,
+  },
+  muted: {
+    type: Boolean,
+    default: "true",
+  },
+  adminid: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = model("Material", MaterialSchema);
